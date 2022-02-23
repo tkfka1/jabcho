78d57bd1491a8a1f4db3e4b61c91026aa27449fd
tkfka1 <36651040+tkfka1@users.noreply.github.com>
2022-02-23 23:13:31 +0900

    Add files via upload

--- app.py
+++ app.py
@@ -1,52 +1,42 @@
 from flask import Flask, render_template, jsonify, request
 app = Flask(__name__)
-
-import requests
-from bs4 import BeautifulSoup
-
 from pymongo import MongoClient
-client = MongoClient('localhost', 27017)
-db = client.dbjungle
+#client = MongoClient('localhost', 27017)
+client = MongoClient('mongodb://jungletest:test@3.34.185.1', 27017)
+db = client.alonememo2
 
 ## HTML을 주는 부분
 @app.route('/')
 def home():
    return render_template('index.html')
-
-@app.route('/memo', methods=['GET'])
-def read_articles():
-    # 1. mongoDB에서 _id 값을 제외한 모든 데이터 조회해오기 (Read)
-    result = list(db.articles.find({}, {'_id': 0}))
-    # 2. articles라는 키 값으로 article 정보 보내주기
-    return jsonify({'result': 'success', 'articles': result})
-
-@app.route('/memo', methods=['POST'])
-def post_article():
-    # 1. 클라이언트로부터 데이터를 받기
-    url_receive = request.form['url_give']  # 클라이언트로부터 url을 받는 부분
-    comment_receive = request.form['comment_give']  # 클라이언트로부터 comment를 받는 부분
-
-    # 2. meta tag를 스크래핑하기
-    headers = {
-        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.86 Safari/537.36'}
-    data = requests.get(url_receive, headers=headers)
-    soup = BeautifulSoup(data.text, 'html.parser')
-
-    og_image = soup.select_one('meta[property="og:image"]')
-    og_title = soup.select_one('meta[property="og:title"]')
-    og_description = soup.select_one('meta[property="og:description"]')
-
-    url_title = og_title['content']
-    url_description = og_description['content']
-    url_image = og_image['content']
-
-    article = {'url': url_receive, 'title': url_title, 'desc': url_description, 'image': url_image,
-               'comment': comment_receive}
-
-    # 3. mongoDB에 데이터를 넣기
-    db.articles.insert_one(article)
-
+## 메모 get으로 읽어보내기
+@app.route("/memo", methods=["GET"])
+def readMemo():
+    result = list(db.memo.find({}, {'_id': 0}))
+    return jsonify({'result': 'success', 'memos': result})
+## post로 얻어온 값을 DB에 작성
+@app.route("/memo", methods=["POST"])
+def writeMemo():
+    title = request.form["title"]
+    contents = request.form["contents"]
+    result = db.memo.insert_one({"title" : title, "contents" : contents})
+    return jsonify({'result': 'success'})
+## post로 얻어온 값을 DB에 수정
+@app.route("/memoUpdate", methods=["POST"])
+def memoUpdate():
+    memoNum = int(request.form["cardNum"])
+    title = request.form["title"]
+    contents = request.form["contents"]
+    memoList = list(db.memo.find({}, {'title': 0 , 'contents': 0}))
+    result = db.memo.update_one(memoList[memoNum], {"$set": {"title": title , 'contents' : contents}})
+    return jsonify({'result': 'success'})
+## post로 얻어온 값으로 데이터베이스에 있는 메모 삭제
+@app.route("/memoDelete", methods=["POST"])
+def memoDelete():
+    memoNum = int(request.form["cardNum"])
+    memoList = list(db.memo.find({}, {'title': 0 , 'contents': 0}))
+    result = db.memo.delete_one(memoList[memoNum])
     return jsonify({'result': 'success'})
 
-if __name__ == '__main__':
-   app.run('0.0.0.0',port=5000,debug=True)
+if __name__ == "__main__":
+    app.run('0.0.0.0', port=5000, debug=True)
